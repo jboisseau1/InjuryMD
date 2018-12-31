f5f9bf6f06448605c91abe47aa7abf1f59b700cb
Jacob Boisseau <jacobboisseau@gmail.com>
2018-12-31 11:20:05 -0500

    added vuesax

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+
+import Vuesax from 'vuesax';
+
+import 'vuesax/dist/vuesax.css'; //Vuesax styles
+Vue.use(Vuesax);
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
